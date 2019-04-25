78269b9bbacbc8d2e204a76f1afefb003d991690
simona454 <simona454@gmail.com>
2019-04-26 00:14:53 +0300

    Added simple functionality for switching between levels. Highlighted the path and object with simple effect.(Temporary)  Addicted project and scene settings.

---
+++ GyroBoard/Assets/Scripts/Target.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Target : MonoBehaviour
+{
+    private Scene scene;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scene = SceneManager.GetActiveScene();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        //If a gameObject with the tag "Player" enters this trigger, load a scene.
+        if (other.gameObject.tag == "Player")
+        {
+            if(scene.name == "FlatSurface")
+                Application.LoadLevel("SlopedPath");
+            else if (scene.name == "SlopedPath")
+                Application.LoadLevel("End");
+        }
+    }
+}

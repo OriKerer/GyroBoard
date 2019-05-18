511dda335b597617a71a499fffa34d1f6d0c7b18
simona454 <simona454@gmail.com>
2019-05-19 00:35:58 +0300

    Started working on slides, ramps, speeders, samples and other s.

---
+++ GyroBoard/Assets/Scripts/Speedup.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Speedup : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        other.gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(30,30,0));
+        print("speedup");
+    }
+}

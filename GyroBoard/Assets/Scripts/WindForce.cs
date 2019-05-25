bfeb2d6e3cb44540d618d5faf5b579cf01d0dfe7
simona454 <simona454@gmail.com>
2019-05-25 18:58:35 +0300

    Added wind

---
+++ GyroBoard/Assets/Scripts/WindForce.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindForce : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void OnTriggerStay(Collider other)
+    {
+        Vector3 movement = new Vector3(100, 0, 100);
+        Vector3 v3 = transform.TransformDirection(movement);
+        other.gameObject.GetComponent<Rigidbody>().AddForce(v3);
+        print("Bounce");
+    }
+
+}

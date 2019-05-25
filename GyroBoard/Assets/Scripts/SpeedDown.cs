7b0f8b35db4b0dff6474468d16c1f9538aaad549
simona454 <simona454@gmail.com>
2019-05-25 21:48:19 +0300

    Added puddle

--- GyroBoard/Assets/Scripts/SpeedDown.cs
+++ GyroBoard/Assets/Scripts/SpeedDown.cs
@@ -19,7 +19,7 @@ public class SpeedDown : MonoBehaviour
     private void OnTriggerStay(Collider other)
     {
         Rigidbody rb = other.GetComponent<Rigidbody>();
-        rb.AddForce(-rb.velocity*5);
+        rb.AddForce(-rb.velocity*4);
         print("Sand");
     }
 }
---
+++ GyroBoard/Assets/Scripts/WetSurface.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WetSurface : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        Rigidbody rb = other.GetComponent<Rigidbody>();
+        rb.AddForce(rb.velocity * 3);
+        print("Water");
+    }
+}
